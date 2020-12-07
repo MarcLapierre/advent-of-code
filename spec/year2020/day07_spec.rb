59b405f5221fcee43c2eae77cf5fb106ac39e0c5
Marc Lapierre <marc.lapierre@shopify.com>
2020-12-07 10:17:00 -0500

    2020 day 7

--- spec/year2020/day07_spec.rb
+++ spec/year2020/day07_spec.rb
@@ -1,13 +1,27 @@
 require 'spec_helper'
 
 RSpec.describe Year2020::Day07 do
-  it "solves part1" do
-    d = Year2020::Day07.new
-    expect(d.part1('some_input')).to eq('expected_result')
-  end
+  # it "solves part1" do
+  #   d = Year2020::Day07.new
+  #   expect(d.part1('some_input')).to eq('expected_result')
+  # end
 
   it "solves part2" do
     d = Year2020::Day07.new
-    expect(d.part2('some_input')).to eq(nil)
+    expect(d.part2(input)).to eq(126)
+  end
+
+  private
+
+  def input
+    <<~HEREDOC
+    shiny gold bags contain 2 dark red bags.
+    dark red bags contain 2 dark orange bags.
+    dark orange bags contain 2 dark yellow bags.
+    dark yellow bags contain 2 dark green bags.
+    dark green bags contain 2 dark blue bags.
+    dark blue bags contain 2 dark violet bags.
+    dark violet bags contain no other bags.
+    HEREDOC
   end
 end
--- src/year2020/day07.rb
+++ src/year2020/day07.rb
@@ -1,11 +1,65 @@
 module Year2020
   class Day07
+    TARGET = 'shiny gold'
+
     def part1(input)
-      "expected_result"
+      bags = parse_input1(input)
+      targets = [TARGET]
+      loop do
+        results = find_bags_containing(targets, bags)
+        break if results.count == targets.count
+        targets = (targets + results).uniq
+      end
+      targets.count
     end
 
     def part2(input)
-      nil
+      bags = parse_input2(input)
+      count_bags_for(TARGET, bags)
+    end
+
+    private
+
+    def parse_input1(input)
+      input.chomp.split("\n").map do |bag|
+        if bag.index('no other bags')
+          color = /^(.+) bags contain/.match(bag)[1]
+          [color, []]
+        else
+          matches = /^(.+) bags contain( \d+ (.+)[,\.])+$/.match(bag)
+          color = matches[1]
+          contents = matches[2].split(',').map { |x| / ([a-z ]+) bags?/.match(x)[1] }
+          [color, contents]
+        end
+      end.to_h
+    end
+
+    def parse_input2(input)
+      input.chomp.split("\n").map do |bag|
+        if bag.index('no other bags')
+          color = /^(.+) bags contain/.match(bag)[1]
+          [color, []]
+        else
+          matches = /^(.+) bags contain( \d+ (.+)[,\.])+$/.match(bag)
+          color = matches[1]
+          contents = matches[2].split(',').map do |x| 
+            matches = / (\d+) ([a-z ]+) bags?/.match(x) 
+            Array(matches[2]) * matches[1].to_i
+          end.reduce(:+)
+          [color, contents]
+        end
+      end.to_h
+    end
+
+    def find_bags_containing(targets, bags)
+      bags.select do |key, values|
+        (targets & values).any? if values
+      end.keys
+    end
+
+    def count_bags_for(color, bags)
+      # this should be optimized with memoization in a real app but...
+      bags[color].count + bags[color].map { |c| count_bags_for(c, bags) }.sum
     end
   end
 end
